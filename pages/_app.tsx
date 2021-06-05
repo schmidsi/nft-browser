50962d5283dfeb0332bd8a22f631bcaef840b3a1
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-05 12:50:19 +0200

    Add apollo pointing to NFT subgraph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../apollo/client'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,38 @@
+import { gql, useQuery } from '@apollo/client'
 import Head from 'next/head'
 
-export default function Home() {
+const useNFTs = ({ address }: { address: String }) => {
+  const { loading, error, data } = useQuery(
+    gql`
+      query NFTs($address: String!) {
+        account(id: $address) {
+          id
+          tokens {
+            id
+            uri
+            registry {
+              symbol
+              name
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: { address: address.toLowerCase() },
+    },
+  )
+
+  return { loading, error, data }
+}
+
+const Home = () => {
+  const { loading, error, data } = useNFTs({
+    address: '0x546457BBddf5e09929399768ab5a9d588cb0334d',
+  })
+
+  console.log({ loading, error, data })
+
   return (
     <div>
       <Head>
@@ -9,11 +41,22 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>
-          Hi
-        </h1>
+        <input type="text" value="0x546457BBddf5e09929399768ab5a9d588cb0334d"></input>
       </main>
+      <style jsx>{`
+        main {
+          width: 300px;
+          position: relative;
+          margin: 2em auto;
+        }
 
+        input {
+          width: 100%;
+          font-family: monospace;
+        }
+      `}</style>
     </div>
   )
 }
+
+export default Home
