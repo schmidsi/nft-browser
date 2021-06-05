24f1052e3ead96049b1743e043996d998eddc679
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-05 15:18:04 +0200

    Display last traded NFTs on Home

---
+++ hooks/useLastTradedNFTs.ts
@@ -0,0 +1,27 @@
+import { gql, useQuery } from '@apollo/client'
+
+export const useLastTradedNFTs = () => {
+  const { loading, error, data } = useQuery(
+    gql`
+      {
+        transfers(first: 20, orderBy: timestamp, orderDirection: desc) {
+          id
+          timestamp
+          token {
+            id
+            uri
+            registry {
+              symbol
+              name
+            }
+          }
+        }
+      }
+    `,
+    { pollInterval: 5000 },
+  )
+
+  const nfts = data?.transfers?.map((transfer) => transfer.token) || []
+
+  return { loading, error, nfts }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import Head from 'next/head'
 
-import { useNFTs } from '../hooks/useNFTs'
+import { useLastTradedNFTs } from '../hooks/useLastTradedNFTs'
 import { Token } from '../components/Token'
 import { Form } from '../components/Form'
 
 const Home = () => {
-  const { loading, error, nfts } = useNFTs({
-    address: '0x546457BBddf5e09929399768ab5a9d588cb0334d',
-  })
+  const { loading, error, nfts } = useLastTradedNFTs()
 
   return (
     <div>
