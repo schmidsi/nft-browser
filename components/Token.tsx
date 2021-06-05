75da29d3f662b4a58576cb3a9e45fe9ceb3b5117
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-05 14:18:52 +0200

    Refactor Token and useNFTs to own files

---
+++ components/Token.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import fetch from 'cross-fetch'
+
+export const Token = ({ id, uri, registry }) => {
+  const [image, setImage] = useState('')
+
+  useEffect(() => {
+    if (uri) {
+      fetch(uri).then(
+        async (response) => {
+          const text = await response.text()
+          try {
+            const json = JSON.parse(text)
+
+            setImage(json.image || json.image_url)
+            console.log('json', json)
+          } catch (error) {
+            console.log('text', text)
+          }
+        },
+        (reason) => console.warn(reason),
+      )
+    }
+  }, [uri])
+
+  return (
+    <div>
+      {id} {registry.name}
+      <img src={image} />
+      <style jsx>{`
+        div {
+          position: relative;
+          display: inline-block;
+          height: 140px;
+          width: 140px;
+          margin: 5px;
+          overflow: hidden;
+        }
+
+        img {
+          position: absolute;
+          top: 0;
+          left: 0;
+          max-width: 100%;
+          max-height: 100%;
+        }
+      `}</style>
+    </div>
+  )
+}
---
+++ hooks/useNFTs.tsx
@@ -0,0 +1,28 @@
+import { gql, useQuery } from '@apollo/client'
+
+export const useNFTs = ({ address }: { address: String }) => {
+  const { loading, error, data } = useQuery(
+    gql`
+      query NFTs($address: String!) {
+        account(id: $address) {
+          id
+          tokens {
+            id
+            uri
+            registry {
+              symbol
+              name
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: { address: address.toLowerCase() },
+    },
+  )
+
+  const nfts = data?.account?.tokens || []
+
+  return { loading, error, nfts }
+}
