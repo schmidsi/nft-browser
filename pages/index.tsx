310adbff5659ad0fd0ebb1a9df0dc1f572866a12
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-05 14:04:38 +0200

    Load and display image

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,17 @@ const Token = ({ id, uri, registry }) => {
   useEffect(() => {
     if (uri) {
       fetch(uri).then(
-        (result) => console.log(id, uri, result),
+        async (response) => {
+          const text = await response.text()
+          try {
+            const json = JSON.parse(text)
+
+            setImage(json.image || json.image_url)
+            console.log('json', json)
+          } catch (error) {
+            console.log('text', text)
+          }
+        },
         (reason) => console.warn(reason),
       )
     }
@@ -45,13 +55,23 @@ const Token = ({ id, uri, registry }) => {
   return (
     <div>
       {id} {registry.name}
+      <img src={image} />
       <style jsx>{`
         div {
+          position: relative;
           display: inline-block;
           height: 140px;
           width: 140px;
           margin: 5px;
           overflow: hidden;
+        }
+
+        img {
+          position: absolute;
+          top: 0;
+          left: 0;
+          max-width: 100%;
+          max-height: 100%;
         }
       `}</style>
     </div>
