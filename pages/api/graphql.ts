86552912cae4948327aebd61a862365d48e67ef7
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-08 20:50:07 +0200

    Add mesh as /api/graph route

---
+++ pages/api/graphql.ts
@@ -0,0 +1,21 @@
+const { findAndParseConfig } = require('@graphql-mesh/config')
+const { getMesh } = require('@graphql-mesh/runtime')
+import { ApolloServer } from 'apollo-server-micro'
+
+const createApolloServer = async () => {
+  const meshConfig = await findAndParseConfig()
+  const { schema, contextBuilder } = await getMesh(meshConfig)
+  const apolloServer = new ApolloServer({ schema, context: contextBuilder })
+  return apolloServer
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default async (req, res) => {
+  const apolloServer = await createApolloServer()
+  return apolloServer.createHandler({ path: '/api/graphql' })(req, res)
+}
