14b57188c7db6d55b79c020083b5273e8b524c66
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-12 13:24:53 +0200

    Move to config.js mode

---
+++ mesh.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  require: ['ts-node/register/transpile-only'],
+  sources: [
+    {
+      name: 'EIP721',
+      handler: {
+        graphql: {
+          endpoint: 'https://api.thegraph.com/subgraphs/name/amxx/eip721-subgraph',
+        },
+      },
+    },
+    {
+      name: 'EIP1155',
+      handler: {
+        graphql: {
+          endpoint: 'https://api.thegraph.com/subgraphs/name/amxx/eip1155-subgraph',
+        },
+      },
+    },
+  ],
+  transforms: [
+    {
+      namingConvention: {
+        typeNames: 'pascalCase',
+        enumValues: 'camelCase',
+        fieldNames: 'camelCase',
+      },
+    },
+  ],
+  additionalTypeDefs:
+    'enum Standard {\n' +
+    '  erc721\n' +
+    '  erc1155\n' +
+    '}\n' +
+    'extend type Token {\n' +
+    '  standard: Standard!\n' +
+    '}\n',
+  additionalResolvers: ['./mesh/additionalResolvers.ts'],
+}
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,36 +1,10 @@
 import path from 'path'
-import { validateConfig, processConfig } from '@graphql-mesh/config'
+import { validateConfig, processConfig, findAndParseConfig } from '@graphql-mesh/config'
 import { getMesh } from '@graphql-mesh/runtime'
 import { ApolloServer } from 'apollo-server-micro'
 
-import meshConfigYaml from '../../meshrc.yaml'
-
-const preImports = {
-  'ts-node/register/transpile-only': require('ts-node/register/transpile-only'),
-  [`${process.cwd()}/mesh/additionalResolvers.ts`]: require('../../mesh/additionalResolvers'),
-  // [`${process.cwd()}/namingConvention`]: require('@graphql-mesh/transform-naming-convention'),
-}
-
-console.log(meshConfigYaml)
-
-const importFn = (moduleId: string) => {
-  if (preImports[moduleId]) {
-    console.log('importFn pre imported', moduleId)
-    return preImports[moduleId]
-  } else {
-    console.log('importFn not pre imported', moduleId)
-    return import(moduleId)
-  }
-}
-
 const createApolloServer = async () => {
-  validateConfig(meshConfigYaml)
-  console.log(process.cwd(), __dirname, path.join(__dirname, '../..'))
-  const meshConfig = await processConfig(meshConfigYaml, {
-    dir: process.cwd(),
-    importFn,
-    // dir: path.join(__dirname, '../..'),
-  })
+  const meshConfig = await findAndParseConfig()
   const { schema, contextBuilder } = await getMesh(meshConfig)
   const apolloServer = new ApolloServer({
     schema,
