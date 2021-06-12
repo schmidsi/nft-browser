5ab0c79c5ae5bbfa555b0ad291e597ac3946beb5
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-12 12:34:32 +0200

    Manually parse meshrc and load it directly

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,19 @@
-const { findAndParseConfig } = require('@graphql-mesh/config')
-const { getMesh } = require('@graphql-mesh/runtime')
+import path from 'path'
+import { validateConfig, processConfig } from '@graphql-mesh/config'
+import { getMesh } from '@graphql-mesh/runtime'
 import { ApolloServer } from 'apollo-server-micro'
 
+import meshConfigYaml from '../../meshrc.yaml'
+
+console.log(meshConfigYaml)
+
 const createApolloServer = async () => {
-  const meshConfig = await findAndParseConfig()
+  validateConfig(meshConfigYaml)
+  console.log(process.cwd(), __dirname, path.join(__dirname, '../..'))
+  const meshConfig = await processConfig(meshConfigYaml, {
+    dir: process.cwd(),
+    // dir: path.join(__dirname, '../..'),
+  })
   const { schema, contextBuilder } = await getMesh(meshConfig)
   const apolloServer = new ApolloServer({
     schema,
