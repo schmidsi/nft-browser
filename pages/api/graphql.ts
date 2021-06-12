a44f5dbadc64b13ac96868295d61f1fb2d1c60d1
Simon Emanuel Schmid <simon@unchained.shop>
2021-06-12 14:06:22 +0200

    Readd importFn and perImports

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,9 +4,27 @@ import { ApolloServer } from 'apollo-server-micro'
 
 import rawMeshConfig from '../../mesh.config'
 
+const preImports = {
+  typescript: require('typescript'),
+  '@graphql-mesh/transform-naming-convention': require('@graphql-mesh/transform-naming-convention'),
+  '@graphql-mesh/graphql': require('@graphql-mesh/graphql'),
+  'ts-node/register/transpile-only': require('ts-node/register/transpile-only'),
+  [`${process.cwd()}/mesh/additionalResolvers.ts`]: require('../../mesh/additionalResolvers'),
+}
+
+const importFn = (moduleId: string) => {
+  if (preImports[moduleId]) {
+    console.log('importFn pre imported', moduleId)
+    return preImports[moduleId]
+  } else {
+    console.log('importFn not pre imported', moduleId)
+    return import(moduleId)
+  }
+}
+
 const createApolloServer = async () => {
   validateConfig(rawMeshConfig)
-  const meshConfig = await processConfig(rawMeshConfig, { dir: process.cwd() })
+  const meshConfig = await processConfig(rawMeshConfig, { dir: process.cwd(), importFn })
   const { schema, contextBuilder } = await getMesh(meshConfig)
   const apolloServer = new ApolloServer({
     schema,
