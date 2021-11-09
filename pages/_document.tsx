f5832173205f231ffb3b1ca2a744c2c34b4f6b77
Simon Emanuel Schmid <simon@edgeandnode.com>
2021-11-09 21:25:30 +0100

    Revert "Add Zendesk help widget"
    This reverts commit dec6822de6167e29288516fc045ede6d2837b36f.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,12 +23,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script
-            id="ze-snippet"
-            src="https://static.zdassets.com/ekr/snippet.js?key=e71b07ff-2691-45dc-980f-cf6c0aad37b6"
-          >
-            {' '}
-          </script>
         </body>
       </Html>
     )
